2a39aa36b1062c8b7e9c3c0a8ef43d75442c4419
TegarXLu <botdimas67@gmail.com>
2025-10-02 19:32:18 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="SuiKernel"
+KERNEL_NAME="TegarXLu-OSS"
 # Kernel Build variables
-USER="KanagawaYamada"
+USER="TegarXLu"
 HOST="HoshimachiSuisei"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
-ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
-ANYKERNEL_BRANCH="gki"
+ANYKERNEL_REPO="https://github.com/TegarXLu/AnyKernel3"
+ANYKERNEL_BRANCH="main"
 # Kernel Source
-KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
-KERNEL_BRANCH="suikernel-main"
+KERNEL_REPO="https://github.com/marble-indonesia/kernel_xiaomi_sm8450"
+KERNEL_BRANCH="update_kernel"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
 # Clang
-CLANG_URL="https://github.com/mikeNG/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
-CLANG_BRANCH="lineage-20.0"
+CLANG_URL="https://github.com/ZyCromerZ/Clang"
+CLANG_BRANCH="22.0.0git-20250905"
 # Zip name
 # Format: Kernel_name-Linux_version-Variant-Build_date
 ZIP_NAME="$KERNEL_NAME-KVER-VARIANT-BUILD_DATE.zip"
